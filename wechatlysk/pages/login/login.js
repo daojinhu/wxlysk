d71ee234fe12fa87284a291e69afe65ed74d530e
dunconhu <1476847689@qq.com>
2018-05-07 23:06:09 +0800

    updatecode

--- wechatlysk/pages/login/login.js
+++ wechatlysk/pages/login/login.js
@@ -20,20 +20,36 @@ Page({
 
   // 登录 
   login: function () {
-    if (this.data.phone.length == 0 || this.data.password.length == 0) {
+    var phone = this.data.phone;
+    var password = this.data.password;
+
+    if (this.data.phone.length == 0) {
       wx.showToast({
-        title: '用户名和密码不能为空',
+        title: '用户名不能为空',
         icon: 'loading',
-        duration: 2000
+        duration: 1000
+      });
+      return;
+    }
+    if (this.data.password.length == 0) {
+      wx.showToast({
+        title: '密码不能为空',
+        icon: 'loading',
+        duration: 1000
+      });
+      return;
+    }
+    if (phone != 'admin' || password != 123) {
+      wx.showToast({
+        title: '',
       })
-    } else {
+    }
       // 这里修改成跳转的页面 
       wx.showToast({
         title: '登录成功',
         icon: 'success',
         duration: 2000
       })
-    }
   },
 
   //跳转到注册界面
